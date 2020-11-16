11999b057035746310bbfa2a8a5bc904b7768876
19047433 <19047433@myrp.edu.sg>
2020-11-16 20:32:42 +0800

    last touch

--- App.js
+++ App.js
@@ -42,15 +42,19 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
+              //Exerise 1
               <Welcome/>
+              //Exercise 2
               <MyFirstApp/>
             </View>
+            //exercise 3
             <View style={styles.sectionContainer}>
               <Text>My modules this semester: </Text>
               <SemModule day="Monday" moduleCode="C348"/>
               <SemModule day="Tuesday" moduleCode="C237"/>
               <SemModule day="friday" moduleCode="C308"/>
             </View>
+            //Exercise 4
             <View style={styles.sectionContainer}>
               <Text>Recommended Eats @ RP </Text>
               <Eats name="Sweet Tooth Waffle" location="W6 Level 1, E-canteen"/>
@@ -64,6 +68,7 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Eats name="Ayam Penyet" location="W4/W6 Lawn canteen"/>
             </View>
+            //Mini Project
             <View style={styles.sectionContainer}>
               <Text>World Clock</Text>
               <Clock city="Asia/Singapore" captureTiming={MT.tz("Asia/Singapore").format("H:mm Z")}/>
@@ -77,22 +82,27 @@ const App: () => React$Node = () => {
     </>
   );
 };
+//Exercise 1
 const Welcome = () => {
   return(<Text>Welcome to C308 Web Frameworks!</Text>);
 };
+//exercise 2
 class MyFirstApp extends React.Component{
   render() {
     return(<Text>My First React Native App</Text>);
   }
 }
+//Exercise 3
 const SemModule = (props) => {
   return (<Text>{props.day} - {props.moduleCode}</Text>);
 };
+//Exercise 4
 class Eats extends React.Component{
   render() {
     return(<Text>{this.props.name}{"\n"}{this.props.location}</Text>);
   }
 }
+//Mini Project
 class Clock extends React.Component{
   render(){
     return(<Text>{this.props.city} - {this.props.captureTiming}</Text>);
